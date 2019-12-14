c55426272f77353a8c7f0c874f3f05495037503e
Daniel Hoop <daniel.hoop@gmx.net>
2019-12-14 12:40:57 +0100

    Moved html files into subfolder html

--- src/ch/webserver/CategoryHtmlUpdater.java
+++ src/ch/webserver/CategoryHtmlUpdater.java
@@ -6,6 +6,6 @@ import ch.ffhs.pa.competitionmanager.interfaces.INotifiable;
 public class CategoryHtmlUpdater implements INotifiable {
     @Override
     public void notifyMe() {
-        HtmlPage.writetoHTML(HtmlPage.CategoryIDs(GlobalState.getInstance().getCategoryList().getCategories()).render(), "category_content.txt");
+        HtmlPage.writetoHTML(HtmlPage.CategoryIDs(GlobalState.getInstance().getCategoryList().getCategories()).render(), "html/category_content.txt");
     }
 }
--- src/ch/webserver/RankingHtmlUpdater.java
+++ src/ch/webserver/RankingHtmlUpdater.java
@@ -6,6 +6,6 @@ import ch.ffhs.pa.competitionmanager.interfaces.INotifiable;
 public class RankingHtmlUpdater implements INotifiable {
     @Override
     public void notifyMe() {
-        HtmlPage.writetoHTML(HtmlPage.FullContent(GlobalState.getInstance().getRankingList().getScores()).render(),"content.txt");
+        HtmlPage.writetoHTML(HtmlPage.FullContent(GlobalState.getInstance().getRankingList().getScores()).render(),"html/content.txt");
     }
 }
