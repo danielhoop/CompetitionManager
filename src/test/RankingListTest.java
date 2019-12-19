b47066a408c82f23d83d838178e373fc5bf72f51
Daniel Hoop <daniel.hoop@gmx.net>
2019-12-19 22:18:03 +0100

    Used TestDataPreparator.prepare() in cristians tests to simplify them. All tests are passing!

--- src/test/RankingListTest.java
+++ src/test/RankingListTest.java
@@ -29,56 +29,26 @@ class RankingListTest {
 
     @Test
     void getScores() {
+        TestDataPreparator.prepare();
+
         GlobalState globalState = GlobalState.getInstance();
-        String driverPath, driverName, address, user, password;
-
-        driverPath = "C:/Users/cr_io/IdeaProjects/ninja-warriors2/lib/mysql-connector-java-8.0.17.jar";
-        driverName = "com.mysql.cj.jdbc.Driver";
-        address = "jdbc:mysql://localhost:3306/CompetitionManager?autoReconnect=true&verifyServerCertificate=false&useSSL=true&useJDBCCompliantTimezoneShift=true&useLegacyDatetimeCode=false&serverTimezone=GMT";
-        user = "NinjaWarrior";
-        password = "Asdf-Poiu-0987-1234";
-
-        try {
-            // Prepare database driver and connection to database.
-            System.out.print("Loading database driver...");
-            DynamicDriverLoader.registerDriver(driverPath, driverName);
-            globalState.setDbConnector(new DbConnector(address, user, password));
-            System.out.println(" Done.");
-        } catch (SQLException | ClassNotFoundException | InstantiationException | IllegalAccessException | MalformedURLException | FileNotFoundException e) {
-            JOptionPane.showConfirmDialog(null, "Something went wrong when trying to register the database driver:\n" + e.getMessage(), "Error",  JOptionPane.CANCEL_OPTION, JOptionPane.PLAIN_MESSAGE);
-            System.exit(1);
-        }
-
-        globalState.setEventList(new EventList());
-        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("d/MM/yyyy");
-        Event ev = new Event(1,"Ninja Warriors", LocalDate.parse("11/09/2018", formatter),"11. September 2018","First event organized with software",true);
-        globalState.setEvent(ev);
+        globalState.setEvent(Event.getById(1));
 
         Map<Category, List<Score>> scores = GlobalState.getInstance().getRankingList().getScores();
 
-        LocalTime timeCheck;
-
-
         for (Map.Entry<Category, List<Score>> cat : scores.entrySet()) {
-
-
             Boolean isSorted = true;
-
             Iterator<Score> iter = cat.getValue().iterator();
             Score current, previous = iter.next();
+
             while (iter.hasNext()) {
                 current = iter.next();
                 if (previous.getTimeNeeded().compareTo(current.getTimeNeeded()) > 0) {
                     isSorted = false;
                 }
                 previous = current;
-
-
             }
             assertTrue(isSorted);
-
         }
-
-
     }
 }
